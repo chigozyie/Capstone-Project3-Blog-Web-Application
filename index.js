28704d45093b5353e227dfb0b7a411b87a0a110b
chigozyie <chigouhiara@gmail.com>
2026-01-02 23:37:11 +0100

    Code Fixes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,82 +10,50 @@ app.use(express.static("public"));
 
 let blogs = [];
 
-app.get("/", (req, res) => {
-    res.render("index.ejs", {
-        blogs: blogs
-    })
-});
+// Helper function to find blog by ID
+const findBlog = (id) => blogs.find(b => b.id === parseInt(id));
+const findBlogIndex = (id) => blogs.findIndex(b => b.id === parseInt(id));
+
+app.get("/", (req, res) => res.render("index.ejs", { blogs }));
 
 app.get("/blog/:id", (req, res) => {
-    const blogId = parseInt(req.params.id);
-    const blog = blogs.find(b => b.id === blogId);
-    
-    if (blog) {
-        res.render("blog.ejs", {
-            blog: blog
-        });
-    } else {
-        res.status(404).send("Blog not found");
-        res.send('<a href="/">Go Back</a>');
-    }
+    const blog = findBlog(req.params.id);
+    blog ? res.render("blog.ejs", { blog }) : res.status(404).render("404.ejs", { message: "Blog not found" });
 });
 
 app.post("/create-new-blog", (req, res) => {
-    const blog = {
+    blogs.push({
         id: blogs.length + 1,
         title: req.body.title,
         content: req.body.content,
         date: new Date().toLocaleDateString()
-    };
-    blogs.push(blog);
+    });
     res.redirect("/");
 });
 
 app.get("/edit-blog/:id", (req, res) => {
-    const blogId = parseInt(req.params.id);
-    const blog = blogs.find(b => b.id === blogId);
-    
-    if (blog) {
-        res.render("edit-blog.ejs", {
-            blog: blog
-        });
-    } else {
-        res.status(404).send("Blog not found");
-    }
+    const blog = findBlog(req.params.id);
+    blog ? res.render("edit-blog.ejs", { blog }) : res.status(404).send("Blog not found");
 });
 
 app.post("/blog/:id/update", (req, res) => {
-  
-    const blogId = parseInt(req.params.id);
-    const blogIndex = blogs.findIndex(b => b.id === blogId);
+    const blogIndex = findBlogIndex(req.params.id);
+    if (blogIndex === -1) return res.status(404).send("Blog not found");
     
-    if (blogIndex === -1) {
-      console.log('Blog not found with ID:', blogId);
-      return res.status(404).send("Blog not found");
-    }
-
     blogs[blogIndex] = {
-      id: blogId,
-      title: req.body.title,
-      content: req.body.content,
-      date: blogs[blogIndex].date
+        ...blogs[blogIndex],
+        title: req.body.title,
+        content: req.body.content
     };
-    res.redirect(`/blog/${blogId}`);
-
+    res.redirect(`/blog/${req.params.id}`);
 });
 
 app.post("/blog/:id/delete", (req, res) => {
-    const blogId = parseInt(req.params.id);
-    const blogIndex = blogs.findIndex(b => b.id ===blogId);
-    if(blogIndex === -1){
-        return res.status(404).send("Blog not found");
-    }
+    const blogIndex = findBlogIndex(req.params.id);
+    if (blogIndex === -1) return res.status(404).send("Blog not found");
+    
     blogs.splice(blogIndex, 1);
-    res.render("index.ejs", {
-        blogs: blogs
-    })
+    res.render("index.ejs", { blogs });
 });
 
-app.listen(port, () =>{
-    console.log(`Server listening on port http://localhost:${port}`)
-})
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
